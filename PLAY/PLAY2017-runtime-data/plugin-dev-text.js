175ca491e32a0f33afcc77f2adbf6eb06d0a2a6b
Matthew Boles <mboles@brightcove.com>
2017-04-24 16:49:06 -0400

    PLAY hands-on work

--- PLAY/PLAY2017-runtime-data/plugin-dev-text.js
+++ PLAY/PLAY2017-runtime-data/plugin-dev-text.js
@@ -1,6 +1,7 @@
 videojs.plugin('pluginDevTextName', function(options) {
   var myPlayer = this,
     pluginDevText = document.createElement('p');
+  pluginDevText.style.color = options.textColor;
   pluginDevText.className = 'pluginDevTextClass';
   pluginDevText.innerHTML = options.dynamicText;
   myPlayer.el().appendChild(pluginDevText);
