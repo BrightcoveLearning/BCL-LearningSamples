276339b521402d952e8650599266719b4619e48b
Matthew Boles <mboles@brightcove.com>
2017-05-19 15:17:27 -0400

    WIP PLAY

--- PLAY/PLAY2017-phase-2/plugin-dev-text.js
+++ PLAY/PLAY2017-phase-2/plugin-dev-text.js
@@ -1,7 +1,7 @@
 videojs.plugin('pluginDevTextName', function() {
   var myPlayer = this,
    pluginDevText = document.createElement('p');
-  pluginDevText.className = 'pluginDevTextClass',
+  pluginDevText.className = 'pluginDevTextClass';
   pluginDevText.innerHTML = 'Becoming a plugin developer';
   myPlayer.el().appendChild(pluginDevText);
 });
