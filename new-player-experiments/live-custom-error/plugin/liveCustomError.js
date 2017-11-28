774cfc949134d8193848bf89d6743389f1836547
Matthew Boles <mboles@brightcove.com>
2017-11-28 16:53:22 -0500

    Updating code

--- new-player-experiments/live-custom-error/plugin/liveCustomError.js
+++ new-player-experiments/live-custom-error/plugin/liveCustomError.js
@@ -1,52 +1,39 @@
 videojs.registerPlugin('liveCustomError', function() {
-  var myPlayer = this,
-    // Initialize boolean variable so overlay not shown be default
-    showOverlay = false;
+  var myPlayer = this;
+  
+  // +++ Create a div in which to place HTML content and place image inside +++
+  var newElement = document.createElement('div');
+  newElement.innerHTML = "<img src='//solutions.brightcove.com/bcls/assets/images/please-stand-by.png'>";
 
-  // Define when the overlay should be displayed
-  // and what it will display
-  myPlayer.overlay({
-    overlays: [{
-      "start": "loadstart",
-      "end": "ended",
-      "content": "<img src='http://solutions.brightcove.com/bcls/assets/images/please-stand-by.png'>"
-    }]
-  });
+  // +++ Define options object to be used for modal content +++
+  var options = {};
+  options.content = newElement;
 
-  // Redefine the messages displayed by the error
-  // THIS ERROR WILL NOT BE DISPLAYED unless you don't implement the overlay image
-  // or if the overlay fails to load the image for some reason
-  myPlayer.errors({
-    'errors': {
-      '4': {
-        'headline': 'The Live Event you are trying to watch is either unavailable or has not started',
-        'type': 'MEDIA_UNAVAILABLE',
-      }
-    }
-  });
+  // +++ Create Modal with options object +++
+  var ModalDialog = videojs.getComponent('ModalDialog');
+  var myModal = new ModalDialog(myPlayer, options);
+  myPlayer.addChild(myModal);
 
-  // Listen for an error event
+  // +++Listen for an error event  +++
   myPlayer.on('error', function(err) {
-    // The overlay should only be displayed if the error code is 4 or 2
+    // The Modal should only be displayed if the error code is 4
     // and the duration is NaN (not a number)
     // The following code gets the error code and duration
     var errNo = myPlayer.error().code;
     var duration = myPlayer.duration();
-    // Check if the error code and durations mean not video has loaded
-    if (((errNo == '2') && (isNaN(duration))) || ((errNo == '4') && (isNaN(duration)))) {
-      // If conditions met, set display boolean variable to true
-      showOverlay = true;
+
+    // +++ Check if the error code and duration mean no video has loaded +++
+    if ((errNo == '4') && (isNaN(duration))) {
+      // Hide the error display
+      myPlayer.errorDisplay.hide();
+      // If conditions met show the custom modal
+      myModal.open();
     }
-    // Check if overlay should be displayed
-    if (showOverlay) {
-      // Prevent the default error display from appearing
-      myPlayer.error(null);
-      // Remove the hide-overlay so the image appears
-      myPlayer.removeClass("hide-overlay");
-    } else {
-      // Hide overlay image if based on boolean variable
-      myPlayer.addClass("hide-overlay");
-    }
-  })
+  });
+
+  // +++ If custom modal error closed, show the default error +++
+  myModal.on('modalclose', function() {
+    myPlayer.errorDisplay.show();
+  });
 
 });
