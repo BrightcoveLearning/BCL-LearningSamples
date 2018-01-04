34564d8b14b9a4bc400c64465b33d54e2a66fabd
Matthew Boles <mboles@brightcove.com>
2018-01-04 16:23:43 -0500

    Code update

--- new-player-experiments/coming-next-in-playlist/coming-next-in-playlist.js
+++ new-player-experiments/coming-next-in-playlist/coming-next-in-playlist.js
@@ -1,52 +1,75 @@
-videojs.plugin('comingNextInPlaylist', function () {
-  var myPlayer = this,
+videojs.registerPlugin('comingNextInPlaylist', function () {
+  var myPlayer,
     intervalID,
     playlistInfo,
     timeToDisplayOverlay = 5,
-    lengthOfPlaylist = 0;
+    lengthOfPlaylist = 0,
+    lastVideo = false,
+    nextVideoName,
+    nextThumbnail;
 
-  // Add Overlay to player, which is hidden immediately
-  myPlayer.overlay({
-    overlays: [{
-      content: '<h3>this is the overlay</h3>',
-      start: 'play'
-    }]
+  videojs('myPlayerID').ready(function() {
+    var myPlayer = this;
+
+    // +++ Configure overlay, which is hidden immediately +++
+    myPlayer.overlay({
+      overlays: [{
+        content: '<h3>this is the overlay</h3>',
+        start: 'play',
+        align: 'top-right'
+      }]
+    });
   });
 
+
+  // +++ Get playlist info, hide overly, and play video +++
   // Wait for loadedmetatdata to get playlist info
   // Code in this block only performed once, hence one() method for the event listener
-  myPlayer.one('loadedmetadata', function () {
+  videojs('myPlayerID').one('loadedmetadata', function() {
     myPlayer = this;
     playlistInfo = myPlayer.playlist();
     lengthOfPlaylist = playlistInfo.length;
-    // Set interval to call function every one second
+
+    // +++ Set interval to call function every one second +++
     intervalID = window.setInterval(checkTime, 1000);
-    // Listen for every loadedmetatdata, which means a new video has been loaded in the player
+
+    // +++ Listen for every loadedmetatdata, which means a new video has been loaded in the player +++
     myPlayer.on('loadedmetadata', videoChanged);
     // Hide overlay on initial load of player
     myPlayer.addClass("hide-overlay");
     myPlayer.play();
   });
 
-  // Function called every one second
+  // +++ Define function called every second +++
   function checkTime() {
     if (!myPlayer.paused()) {
+
       // Calculate time remaining in video
       var currentVideoIndex = myPlayer.playlist.currentItem(),
         nextVideoIndex = currentVideoIndex + 1,
         currentVideoDuration = playlistInfo[currentVideoIndex].duration,
         timeRemaining = currentVideoDuration - myPlayer.currentTime();
-      // Assign name of next video, or assign end of playlist note
+
+      // Assign name and thumbnail of next video, or assign end of playlist note
       if (nextVideoIndex < lengthOfPlaylist) {
-        nextVideoName = playlistInfo[nextVideoIndex].name
+        nextVideoName = 'Next video in playlist: ' + playlistInfo[nextVideoIndex].name
+        nextThumbnail = playlistInfo[nextVideoIndex].thumbnail;
       } else {
+        lastVideo = true;
         nextVideoName = 'Last video in playlist'
       };
+
+      // +++ Display overlay if less than 5 seconds left in video +++
       // Display overlay with name or note if near end of video
       // Default time to display note is 5 seconds to end
       if (timeRemaining <= timeToDisplayOverlay) {
-        var theDiv = document.getElementsByClassName("div.vjs-overlay");
-        document.getElementsByClassName("vjs-overlay")[0].innerHTML = nextVideoName;
+        var theDiv = document.getElementsByClassName("vjs-overlay")[0];
+        if (lastVideo) {
+          theDiv.innerHTML = nextVideoName;
+        } else {
+          theDiv.innerHTML = nextVideoName + "</br></br><img src='" + nextThumbnail + "'>";
+        }
+
         myPlayer.removeClass("hide-overlay");
         if (nextVideoIndex >= lengthOfPlaylist) {
           window.clearInterval(intervalID);
--- new-player-experiments/download-video/download-video.js
+++ new-player-experiments/download-video/download-video.js
@@ -1,34 +1,55 @@
-videojs.plugin('downloadVideoPlugin', function() {
+videojs.registerPlugin('downloadVideo', function() {
+  // Create variables and new div, anchor and image for download icon
   var myPlayer = this,
-    videoID,
     videoName,
     totalRenditions,
-    mp4Ara=[],
+    mp4Ara = [],
     highestQuality,
-    controlBar,
-    newElement;
-    
-  videoID = myPlayer.options()['data-video-id'];
-  myPlayer.catalog.getVideo(videoID, function(error, video) {
-    myPlayer.catalog.load(video);
+    spacer,
+    newElement = document.createElement('div'),
+    newLink = document.createElement('a'),
+    newImage = document.createElement('img');
+
+  myPlayer.on('loadstart', function() {
+    //Reinitialize array of MP4 renditions in case used with playlist
+    //This prevents the array having a cumulative list for all videos in playlist
+    mp4Ara = [];
+
+    // +++ Get video name and the MP4 renditions +++
     videoName = myPlayer.mediainfo['name'];
     rendtionsAra = myPlayer.mediainfo.sources;
     totalRenditions = rendtionsAra.length;
+
+    // +++ Loop over videos and extract only MP4 versions +++
     for (var i = 0; i < totalRenditions; i++) {
       if (rendtionsAra[i].container === "MP4" && rendtionsAra[i].hasOwnProperty('src')) {
         mp4Ara.push(rendtionsAra[i]);
       };
     };
-    mp4Ara.sort( function (a,b){
+
+    // +++ Sort the renditions from highest to lowest on size+++
+    mp4Ara.sort(function(a, b) {
       return b.size - a.size;
     });
-    highestQuality= mp4Ara[0].src;
-      
-    newElement = document.createElement('div');
+
+    // +++ Extract the highest rendition +++
+    highestQuality = mp4Ara[0].src;
+
+    // +++ Build the download button element +++
     newElement.id = 'downloadButton';
-    newElement.className = 'downloadStyle vjs-control'; 
-    newElement.innerHTML = "<a href='" + highestQuality + "' download='" + videoName + "'><img src='http://solutions.brightcove.com/bcls/brightcove-player/download-video/file-download.png' /></a>";
-    controlBar = document.getElementsByClassName('vjs-control-bar');
-    controlBar[0].appendChild(newElement);
-  });
+    newElement.className = 'downloadStyle vjs-control';
+    newImage.setAttribute('src', 'http://solutions.brightcove.com/bcls/brightcove-player/download-video/file-download.png');
+    newLink.setAttribute('href', highestQuality);
+    newLink.setAttribute('download', videoName);
+    newLink.appendChild(newImage);
+    newElement.appendChild(newLink);
+
+    // +++ Place the download button +++
+    // Get a handle on the spacer element
+    spacer = myPlayer.controlBar.customControlSpacer.el();
+    // Set the content of the spacer to be right justified
+    spacer.setAttribute("style", "justify-content: flex-end;");
+    // Place the new element in the spacer
+    spacer.appendChild(newElement);
+  })
 });
