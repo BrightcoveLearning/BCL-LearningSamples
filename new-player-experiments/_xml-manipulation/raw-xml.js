7e07eb31504b35c79a140fbac7c0712b495ea5df
Matthew Boles <mboles@brightcove.com>
2018-09-12 12:49:20 -0700

    WIP: chaptered video marks work

---
+++ new-player-experiments/_xml-manipulation/raw-xml.js
@@ -0,0 +1,40 @@
+var rawxml=<item>
+            <title>Brightcove Player Loader</title>
+            <pubDate>Fri, 31 Aug 2018 15:14 -0400</pubDate>
+            <description>In this topic, you will learn how to use the Brightcove Player Loader to load a Brightcove Player using modern build tooling. This library is seen as a tool for more technical customers.</description>
+            <link>https://support.brightcove.com/brightcove-player-loader</link>
+            <author>rcrooks@brightcove.com</author>
+            <category>Brightcove Player</category>
+        </item>
+        <item>
+            <title>Brightcove Player Sample: Get Seeked Time</title>
+            <pubDate>Fri, 31 Aug 2018 15:18 -0400</pubDate>
+            <description>In this topic, you will learn how to get the time to which the user is seeking.</description>
+            <link>https://support.brightcove.com/brightcove-player-sample-get-seeked-time</link>
+            <author>rcrooks@brightcove.com</author>
+            <category>Brightcove Player</category>
+        </item>
+        <item>
+            <title>Pre-release Player</title>
+            <pubDate>Fri, 31 Aug 2018 15:19 -0400</pubDate>
+            <description>In this topic, you will learn how to create a player that is always on the latest pre-release version.</description>
+            <link>https://support.brightcove.com/pre-release-player</link>
+            <author>rcrooks@brightcove.com</author>
+            <category>Brightcove Player</category>
+        </item>
+        <item>
+            <title>Overview: Analytics API v1</title>
+            <pubDate>Fri, 07 Sep 2018 15:11 -0400</pubDate>
+            <description>Added quick reference table for dimension combinations that are supported by the Analytics API.</description>
+            <link>https://support.brightcove.com/overview-analytics-api-v1#supported_dimension_combinations</link>
+            <author>rcrooks@brightcove.com</author>
+            <category>Analytics API</category>
+        </item>
+        <item>
+            <title>Error Handling with the Native SDK for iOS</title>
+            <pubDate>Fri, 07 Sep 2018 15:12 -0400</pubDate>
+            <description>In this topic, you will learn how the Brightcove Native SDK for iOS handles playback errors.</description>
+            <link>https://support.brightcove.com/error-handling-native-sdk-ios</link>
+            <author>rcrooks@brightcove.com</author>
+            <category>Brightcove Player Native SDK for iOS</category>
+        </item>
