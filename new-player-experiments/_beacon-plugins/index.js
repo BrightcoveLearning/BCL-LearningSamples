751c48c72cd4b3f1ae36a3f6768d5174d213ed6d
Matthew Boles <mboles@brightcove.com>
2021-10-27 14:08:46 -0700

    New report

--- new-player-experiments/_beacon-plugins/index.js
+++ new-player-experiments/_beacon-plugins/index.js
@@ -2,11 +2,21 @@
 window.addEventListener("message",
   (event) => {
     const originsAllowed = [
-        'https://beacon-web.ott.us-east-1.qa.deploys.brightcove.com'
-      ]
-      if (originsAllowed.includes(event.origin)) {
-        console.log('event: ', event)
-      }
+      'https://beacon-web.ott.us-east-1.qa.deploys.brightcove.com'
+    ]
+    if (originsAllowed.includes(event.origin)) {
+      console.log('event: ', event);
+      getAddLearnMoreButtonMessage = () => ({
+        event: POST_EVENT_TYPE.detailsPageAddCustomButton,
+        data: {
+          title: 'My Test Button',
+          font_awesome_icon: "fa fa-info-circle",
+          element_id: LEARN_MORE_BTN_ID,
+        },
+      });
+    } else {
+      alert('Invalid origin');
+    }
   },
   false
 );
