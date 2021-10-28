0d22ca96479ceacc18e73ddc9787ee15b5c49d17
Matthew Boles <mboles@brightcove.com>
2021-10-28 13:56:16 -0700

    Code for Beacon Plugins

--- new-player-experiments/_beacon-plugins/index.js
+++ new-player-experiments/_beacon-plugins/index.js
@@ -1,21 +1,31 @@
 // This is startup JS
 window.addEventListener("message",
-  (onBeaconPageChange) => {
+  (event) => {
     const originsAllowed = [
       'https://beacon-web.ott.us-east-1.qa.deploys.brightcove.com'
     ];
     if (originsAllowed.includes(event.origin)) {
-      console.log('event: ', event);
 
-      // Call detailsPageAddCustomButton to add a button to details pages
-      myAddButton = window.postMessage({
-        event: 'detailsPageAddCustomButton',
-        data: {
-          title: 'Test Button',
-          font_awesome_icon: 'fa fa-info-circle',
-          element_id: 'TEST_BTN_ID'
-        }
-      }, window.location.origin);
+      console.log('event: ', event);
+      console.log('event.data.data.page_type: ', event.data.data.page_type);
+      switch(event.data.event) {
+        case 'beforeBeaconPageLoad':
+          if (event.data.data.page_type === 'details') {
+            console.log('in beforepageload');
+            // Call detailsPageAddCustomButton to add a button to details pages
+            myAddButton = window.postMessage({
+              event: 'detailsPageAddCustomButton',
+              data: {
+                title: 'Test Button',
+                font_awesome_icon: 'fa fa-info-circle',
+                element_id: 'TEST_BTN_ID'
+              }
+            }, window.location.origin);
+          }
+          break;
+        default:
+          // code block
+      }
 
     } else {
       alert('Invalid origin');
