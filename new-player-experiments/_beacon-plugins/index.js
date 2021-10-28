3e5107dd3d3dcae5ee7b5b145ef97d61ac6b8907
Matthew Boles <mboles@brightcove.com>
2021-10-28 09:45:59 -0700

    Beacon plugin code

--- new-player-experiments/_beacon-plugins/index.js
+++ new-player-experiments/_beacon-plugins/index.js
@@ -1,19 +1,22 @@
-//this is startup JS
+// This is startup JS
 window.addEventListener("message",
-  (event) => {
+  (onBeaconPageChange) => {
     const originsAllowed = [
       'https://beacon-web.ott.us-east-1.qa.deploys.brightcove.com'
-    ]
+    ];
     if (originsAllowed.includes(event.origin)) {
       console.log('event: ', event);
-      getAddLearnMoreButtonMessage = () => ({
-        event: POST_EVENT_TYPE.detailsPageAddCustomButton,
+
+      // Call detailsPageAddCustomButton to add a button to details pages
+      myAddButton = window.postMessage({
+        event: 'detailsPageAddCustomButton',
         data: {
-          title: 'My Test Button',
-          font_awesome_icon: "fa fa-info-circle",
-          element_id: LEARN_MORE_BTN_ID,
-        },
-      });
+          title: 'Test Button',
+          font_awesome_icon: 'fa fa-info-circle',
+          element_id: 'TEST_BTN_ID'
+        }
+      }, window.location.origin);
+
     } else {
       alert('Invalid origin');
     }
