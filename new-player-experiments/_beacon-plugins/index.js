f714e2069436c6a23e03d973ef216a008702e675
Matthew Boles <mboles@brightcove.com>
2021-11-01 08:40:04 -0700

    New code for Beacon Plugins

--- new-player-experiments/_beacon-plugins/index.js
+++ new-player-experiments/_beacon-plugins/index.js
@@ -8,13 +8,29 @@ window.addEventListener("message",
     if (originsAllowed.includes(event.origin)) {
 
 
-      console.log('event: ', event);
-      console.log('event.data.data.page_type: ', event.data.data.page_type);
-      console.log('event.data.event: ', event.data.event);
       switch(event.data.event) {
-        case 'beforeBeaconPageLoad':
+        case 'onBeaconPageLoad':
           if (event.data.data.page_type === 'details') {
-            console.log('in details page type');
+
+            window.postMessage({
+              event: 'enablePlayerSidePanel',
+            }, window.location.origin);   
+
+
+            //Add to midPageSection
+            var midPageSection = document.getElementById('beacon_details_mid_page_section');
+            var para1 = document.createElement("P");
+            var t1 = document.createTextNode("Lorem ipsum dolor sit amet. Vel quos harum quo obcaecati nostrum et dolore quia id quia voluptatem id odio iure et saepe totam est minima dicta. Id veritatis tenetur est tempora omnis sed quibusdam delectus 33 deleniti labore ut totam totam ut esse autem est consectetur placeat. Et sequi vitae vel praesentium ducimus a consequuntur ipsam id magnam voluptates ea esse eaque et magni illum. Non ipsum quasi id deleniti blanditiis rem consequatur omnis aut tempora voluptatem eum fugit debitis. Qui delectus perspiciatis quo tempore omnis aut dolor modi sit nulla internos ab dolorem sint. Qui accusamus ut asperiores nobis et sequi minus eos omnis omnis et accusantium voluptatem aut quam molestias.");
+            para1.appendChild(t1);
+            midPageSection.appendChild(para1);
+
+            //Add to beacon-web-app-overflow
+            var webAppOverflow = document.getElementById('beacon-web-app-overflow');
+            var para2 = document.createElement("P");
+            var t2 = document.createTextNode("This is beacon-web-app-overflow.");
+            para2.appendChild(t2);
+            webAppOverflow.appendChild(para2);
+
             // Call detailsPageAddCustomButton to add a button to details pages
             window.postMessage({
               event: 'detailsPageAddCustomButton',
@@ -25,7 +41,12 @@ window.addEventListener("message",
               }
             }, window.location.origin);
           }
-          console.log('after postmessage');
+          if (event.data.data.slug === '24811-home') {
+            alert('on home page');
+            window.postMessage({
+              event: 'enablePlayerSidePanel',
+            }, window.location.origin);            
+          }
           break;
         case 'notlookingforthis':
           console.log('notlookingforthis');
