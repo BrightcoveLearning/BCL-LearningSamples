d1a1fd268ac0adeb3a5e94b947e96da6c6c81e68
Matthew Boles <mboles@brightcove.com>
2022-02-18 11:53:44 -0500

    New report and plugin code udpates

---
+++ _beacon-plugins/20220217/20220217130730/var/www/html/bcls/beacon-plugins/side-panel-module.js
@@ -0,0 +1,36 @@
+const openSidePanel = () => {
+  
+  window.postMessage({
+    event: 'enablePlayerSidePanel',
+    data: {}
+  }, window.location.origin);
+
+};
+
+
+const populateSidePanel = (eventObj) => {
+  console.log('eventObj: ', eventObj);
+  var panelArea = document.getElementById('player_side_panel_hook');
+  var content = '{<br>'
+    + ' asset_id: ' + '"' + eventObj.asset_id + '"<br>'
+    + ' bc_account_id: ' + '"' + eventObj.bc_account_id + '"<br>'
+    + ' device: ' + '"' + eventObj.device + '"<br>'
+    + ' page_id: ' + eventObj.page_id + '<br>'
+    + ' page_type: ' + '"' + eventObj.page_type + '"<br>'
+    + ' playlist_id: ' + eventObj.playlist_id + '<br>'
+    + ' slug: ' + '"' + eventObj.slug + '"<br>'
+    + ' user_language: ' + '"' + eventObj.user_language + '"<br>'
+    + ' video_id: ' + '"' + eventObj.video_id + '"<br>'
+    + '}';
+  panelArea.insertAdjacentHTML('afterbegin', content);
+};
+
+
+const clearSidePanel = () => {
+  
+  var panelArea = document.getElementById('player_side_panel_hook');
+  panelArea.innerHTML = '';
+  
+};
+
+export { openSidePanel, populateSidePanel, clearSidePanel };
