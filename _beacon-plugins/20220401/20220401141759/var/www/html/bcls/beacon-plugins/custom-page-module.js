fdd6df26953b259aa36f124879d8585011e5effd
Matthew Boles <mboles@brightcove.com>
2022-04-04 15:27:08 -0400

    More plugin work

---
+++ _beacon-plugins/20220401/20220401141759/var/www/html/bcls/beacon-plugins/custom-page-module.js
@@ -0,0 +1,8 @@
+const populateCustomPage = (pageContent) => {
+  
+  var customPageArea = document.getElementById('custom_page_area');
+  customPageArea.insertAdjacentHTML('afterbegin', pageContent);
+  // <iframe src="https://beacon-help.support.brightcove.com/ott-plugins/index.html" title="Beacon Plugins Index Page"></iframe>
+};
+
+export { populateCustomPage };
---
+++ _beacon-plugins/20220401/20220401143048/var/www/html/bcls/beacon-plugins/data.js
@@ -0,0 +1,8 @@
+var pageContent = '<h2> This is content in the <em>custom page</em> passed as a parameter from a data file</h2><br>'
+  + '<p>You can build your HTML in many ways and the page can contain the content you choose. You are NOT limited to the page types shown in the page layout UI.</p><br>'
+  + '<p>Even use images!</p>'
+  + '<img src="https://solutions.brightcove.com/bcls/beacon-plugins/yachats-far.png">'
+  + '  <iframe src="https://beacon-help.support.brightcove.com/ott-plugins/index.html" title="Beacon Plugins Index Page"></iframe>';
+
+
+export { pageContent };
