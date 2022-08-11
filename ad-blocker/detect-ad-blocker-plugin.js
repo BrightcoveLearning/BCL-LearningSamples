cebaeabe2a529750686cf4e6abacd6525c46bf00
Matthew Boles <mboles@brightcove.com>
2022-08-11 13:16:37 -0700

    more ad blocking testing

--- ad-blocker/detect-ad-blocker-plugin.js
+++ ad-blocker/detect-ad-blocker-plugin.js
@@ -2,6 +2,11 @@ videojs.registerPlugin('detect_ad_blocker', function () {
   var myPlayer = this;
   myPlayer.usingAdBlocker().then(
     hasBlocker => {
-      if (hasBlocker) { alert('You have an ad blocker enabled, which ruins this demonstration'); }
+      if (hasBlocker) { 
+        console.log('adding ad blocker check');
+        var newP = document.createElement("p");
+        newP = '<aside class="bcls-aside bcls-aside--warning language-editable">These ads will not play as you are using an ad blocker.</aside>';
+        document.getElementById("ad-blocker-note").insertAdjacentHTML('afterbegin', newP);
+       }
     });
 });
