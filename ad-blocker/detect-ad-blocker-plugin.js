aefe087806e235f655371b8d210acd0c61caefd8
Matthew Boles <mboles@brightcove.com>
2022-08-12 07:50:47 -0700

    add link to message

--- ad-blocker/detect-ad-blocker-plugin.js
+++ ad-blocker/detect-ad-blocker-plugin.js
@@ -5,7 +5,7 @@ videojs.registerPlugin('detect_ad_blocker', function () {
       if (hasBlocker) { 
         console.log('adding ad blocker check');
         var newP = document.createElement("p");
-        newP = '<aside class="bcls-aside bcls-aside--warning language-editable">These ads will not play as you are using an ad blocker.</aside>';
+        newP = '<aside class="bcls-aside bcls-aside--warning language-editable">These ads will not play as you are using an ad blocker. <a href="https://ssai.support.brightcove.com/vod/guides/ssai-ad-block-detection-and-auto-failover.html">Learn more about using SSAI (server-side ad insertion) fallback</a> when a client side ad blocker is present.</aside>';
         document.getElementById("ad-blocker-note").insertAdjacentHTML('afterbegin', newP);
        }
     });
